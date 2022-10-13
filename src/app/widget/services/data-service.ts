6f4288294b89502162200d11ae0a0875ef3e6749
ZhiyuHu1 <97715273+ZhiyuHu1@users.noreply.github.com>
2022-10-13 09:19:14 +0800

    Update data-service.ts
    Updata data-service.ts

--- src/app/widget/services/data-service.ts
+++ src/app/widget/services/data-service.ts
@@ -35,5 +35,47 @@ export class DataService {
         .then(res => <Data[]>res["sheet1"])
         .then(data => { return [data, Object.keys(data[0])] as const; });
     }
+  
+    getHeatmapData1() {
+        return this.http.get<any>('assets/Processed_heatmap_4.json')
+        .toPromise()
+        .then(res => <number[]>res["BS"])
+        .then(data => { return data; });
+    }
+
+    getHeatmapData2() {
+        return this.http.get<any>('assets/Processed_heatmap_4.json')
+        .toPromise()
+        .then(res => <number[]>res["CPI"])
+        .then(data => { return data; });
+    }
+
+    getHeatmapData3() {
+        return this.http.get<any>('assets/Processed_heatmap_4.json')
+        .toPromise()
+        .then(res => <any[]>res["DATA"])
+        .then(data => { return data; });
+    }
+
+    getHeatmapData4() {
+        return this.http.get<any>('assets/Processed_heatmap_4.json')
+        .toPromise()
+        .then(res => <number[]>res["MAX_MIN"])
+        .then(data => { return data; });
+    }
+
+    getBarData1() {
+        return this.http.get<any>('assets/Processed_compare_0.json')
+        .toPromise()
+        .then(res => <number[]>res["BS"])
+        .then(data => { return data; });
+    }
+
+    getBarData2() {
+        return this.http.get<any>('assets/Processed_compare_0.json')
+        .toPromise()
+        .then(res => <any[]>res["THROUGHPUT"])
+        .then(data => { return data; });
+    }
 
 }
